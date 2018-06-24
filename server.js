c72a8e69e5a423523df5021f2c2119fe8815dccf
lizheng <lizheng808@gmail.com>
2018-06-24 12:11:42 -0700

    heroku deloy

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+ 
 const forceSSL = function() {
   return function (req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
@@ -10,14 +12,13 @@ const forceSSL = function() {
     next();
   }
 }
+app.use(forceSSL());
 // Run the app by serving the static files
 // in the dist directory
-app.use(forceSSL());
 app.use(express.static(__dirname + '/app'));
 // Start the app by listening on the default
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + './index.html'));
+});
 // Heroku port
-
-
-
-app.listen(process.env.PORT || 8080);
-
+app.listen(process.env.PORT || 8000);
